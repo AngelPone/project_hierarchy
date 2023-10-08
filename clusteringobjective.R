01f8e0d1f9972fb64dd788e816938e56c18003ea
Bohan Zhang <zhangbohan@buaa.edu.cn>
2023-10-09 09:25:43 +1100

    clustering objective

---
+++ clusteringobjective.R
@@ -0,0 +1,50 @@
+batch <- 120
+
+data <- readRDS(sprintf("tourism/ets/batch_%s.rds", batch))
+
+objs2 <- list()
+
+for (representor in REPRESENTORS) {
+  objs2[[representor]] <- list()
+  for (distance in DISTANCES) {
+    objs2[[representor]][[distance]] <- foreach(n_clusters = 1:15, .packages = "cluster") %do% {
+      
+      distance_mat <- data$distance[[representor]][[distance]]
+      pam(distance_mat, k = n_clusters, diss=TRUE)$objective
+    }
+  }
+}
+
+for (representor in REPRESENTORS) {
+  for (distance in DISTANCES) {
+    objs2[[representor]][[distance]] <- do.call(rbind, objs2[[representor]][[distance]])
+  }
+}
+for (representor in REPRESENTORS) {
+  for (distance in DISTANCES) {
+    objs[[representor]][[distance]] <- do.call(rbind, objs[[representor]][[distance]])
+  }
+}
+
+
+plot_dt <- NULL
+for (representor in REPRESENTORS) {
+  x <- objs[[representor]][[DISTANCES[1]]]
+  x <- tibble(values=t(apply(x, 1, function(g){g/x[1,]}))[,2], ncluster=1:15, label=DISTANCES[1])
+  for (distance in DISTANCES[2:length(DISTANCES)]) {
+    x2 <- objs[[representor]][[distance]]
+    x2 <- tibble(values = t(apply(x2, 1, function(g){g/x2[1,]}))[,2],
+                 ncluster = 1:15, label = distance)
+    x <- rbind(x, x2)
+  }
+  plot_dt <- rbind(plot_dt, x %>% mutate(representor = .env$representor))
+}
+
+
+library(ggplot2)
+ggplot(plot_dt) +
+  geom_line(aes(x=ncluster, y=values, color = label, group = label)) +
+  facet_wrap(~representor)
+
+
+
